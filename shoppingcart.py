677e2cf577d69a45c524ab580e87881e0a78dcf3
padmapriyakovvuri <126962992+padmapriyakovvuri@users.noreply.github.com>
2023-05-29 22:08:21 +0200

    shoppingcart.py

---
+++ shoppingcart.py
@@ -0,0 +1,21 @@
+ #shopping cart program
+foods =[]
+prices=[]
+items = 0
+total = 0
+while True:
+  food =input("enter a food to buy( x to quit): ")
+  if food.lower()=="x":
+    break
+  else:
+    price=float( input(f" enter the price of {food}: "))
+    foods.append(food)
+    prices.append(price)
+    items += 1
+print("..... YOUR CART.....")
+for i in range(items):
+  print(foods[i])
+  print(str(prices[i])+"€")
+  total+=prices[i]
+    
+print("your total is:"+str(total)+"€")
